5053a2e24cf547ea487a240c3a2674318d9099ba
Victor Luiz <victorluizcefet@gmail.com>
2020-11-25 09:16:08 -0300

    Alteração na classe Item

--- src/application/Teste.java
+++ src/application/Teste.java
@@ -1,15 +1,7 @@
 package application;
 
-import br.com.lojinha.dao.ItemDAO;
-import br.com.lojinha.dao.LotDAO;
-import br.com.lojinha.dao.ProductDAO;
 import br.com.lojinha.dao.UserDAO;
-import br.com.lojinha.model.Item;
-import br.com.lojinha.model.Lot;
-import br.com.lojinha.model.Product;
 import br.com.lojinha.model.User;
-
-import java.util.Calendar;
 
 public class Teste {
     public static void main(String[] args) {
--- src/br/com/lojinha/model/Item.java
+++ src/br/com/lojinha/model/Item.java
@@ -1,15 +1,9 @@
 package br.com.lojinha.model;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import java.util.Objects;
-
-@Entity
 public class Item {
 
-    @ManyToOne
     private Product product;
-
     private double priceItem;
     private int quantityItem;
 
@@ -17,10 +11,6 @@ public class Item {
         this.product = product;
         this.priceItem = priceItem;
         this.quantityItem = quantityItem;
-    }
-
-    public Item(){
-
     }
 
     public Product getProduct() {
